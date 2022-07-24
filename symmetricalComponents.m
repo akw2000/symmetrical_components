ec21d8adf067a845d4a077cf395ea9d7bb971b1a
Kavindu Warnakulasuriya <86293207+akw2000@users.noreply.github.com>
2022-07-24 22:23:44 +0530

    Update symmetricalComponents.m

--- symmetricalComponents.m
+++ symmetricalComponents.m
@@ -6,18 +6,13 @@ b = x + i*y;
 [x,y] = pol2cart(deg2rad(60),3);
 c = x + i*y;
 ph=[a;b;c;];
-
+% lambda matrix
 [x,y] = pol2cart(deg2rad(120),1);
 ap= x + i*y;
 [x,y] = pol2cart(deg2rad(-120),1);
 ap2= x + i*y;
 l=[1,1,1;1,ap2,ap;1,ap,ap2;];
-
+%to find symmetrical components if phase components given
 out = inv(l)*ph;
+%to find phase components if symmetrical components given
 out = l*ph;
-
-% complex ====================================
-a = x + i*y;
-b = x + i*y;
-c = x + i*y;
-ph=[a;b;c;];
